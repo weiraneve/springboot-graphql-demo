7f9c8374d44008abff2ba698df2cac952b5b9034
weiran1999 <907221539@qq.com>
2023-03-20 12:07:49 +0800

    feat: add plugin

--- src/main/java/com/weiran/graphql/GraphqlApplication.kt
+++ src/main/java/com/weiran/graphql/GraphqlApplication.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-open class GraphqlApplication
+class GraphqlApplication
 
 fun main(args: Array<String>) {
     runApplication<GraphqlApplication>(*args)
